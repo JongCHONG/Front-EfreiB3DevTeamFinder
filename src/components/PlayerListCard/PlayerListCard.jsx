3a67168d71837babbac0c35f489796a20ed766d2
jochong27 <jochong27@gmail.com>
2023-06-22 22:14:11 +0200

    refactor code

--- src/components/PlayerListCard/PlayerListCard.jsx
+++ src/components/PlayerListCard/PlayerListCard.jsx
@@ -4,20 +4,22 @@ import PlayerListStyles from "../PlayerListCard/PlayerListCard.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
- 
+
 const PlayerListCard = ({ player }) => {
   return (
-      <div className={PlayerListStyles.playerCard}>
-        <div className={PlayerListStyles.playersContainer}>
-        <FontAwesomeIcon icon={faCircleUser} size='lg' />
-            <div className={PlayerListStyles.justifyBetween}>
-              <Link to={`/profil/${player._id}`}><span style={{width:"250px"}}>{player.username}</span></Link>
-              <span style={{width:"200px"}}>Rank: {player.rank}</span>
-              <span style={{width:"400px"}}>Discord: {player.discord}</span>
-              <span>Région: {player.region}</span>
-            </div>
+    <div className={PlayerListStyles.playerCard}>
+      <div className={PlayerListStyles.playersContainer}>
+        <FontAwesomeIcon icon={faCircleUser} size="lg" />
+        <div className={PlayerListStyles.justifyBetween}>
+          <span style={{ width: "250px" }}>
+            <Link to={`/profil/${player._id}`}>{player.username}</Link>
+          </span>
+          <span style={{ width: "200px" }}>Rank: {player.rank}</span>
+          <span style={{ width: "400px" }}>Discord: {player.discord}</span>
+          <span>Région: {player.region}</span>
         </div>
       </div>
+    </div>
   );
 };
 
--- src/pages/UserProfilePage/UserProfilePage.jsx
+++ src/pages/UserProfilePage/UserProfilePage.jsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect, useRef } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Fade from "react-reveal/Fade";
-import ScrollToTopButton from "../../components/ScrollToTop/ScrollToTop";
-import UserProfilePageStyles from "../UserProfilePage/UserProfilePage.module.scss";
-import ProfilCard from "../../components/ProfilCard/ProfilCard";
 import { useParams } from "react-router-dom";
+import Fade from "react-reveal/Fade";
+import moment from "moment";
+
+import UserProfilePageStyles from "../UserProfilePage/UserProfilePage.module.scss";
+
 import TemplatePage from "../../components/TemplatePage/TemplatePage";
 import defaultAvatar from "../../assets/images/logo-announcement-default.png";
 import AnnouncementCard from "../../components/AnnouncementCard/AnnouncementCard";
-import moment from "moment";
-import { getAnnouncements } from "../../utils/helpers";
 import Button from "../../components/Button/Button";
+
+import { getAnnouncements } from "../../utils/helpers";
 
 const UserProfilePage = () => {
   const [userInfo, setUserInfo] = useState(null);
@@ -20,33 +20,31 @@ const UserProfilePage = () => {
   const [announcements, setAnnouncements] = useState(null);
 
   useEffect(() => {
-    const fetchAnnouncements = async () => {
-      const data = await getAnnouncements();
-      setAnnouncements(data);
-    };
-    fetchAnnouncements();
-  }, []);
-
-  console.log(id);
-  useEffect(() => {
     const fetchUserInfo = async () => {
       try {
         const response = await fetch(`http://localhost:5000/users/${id}`);
         const data = await response.json();
-        console.log("data", data);
         setUserInfo(data);
-        console.log("userinfo", userInfo);
       } catch (error) {
         console.log(error);
       }
     };
+
+    const fetchAnnouncements = async () => {
+      const announcementsData = await getAnnouncements();
+      const filteredAnnouncements = announcementsData?.filter(
+        (announcement) => announcement.user?._id === userInfo?._id
+      );
+
+      setAnnouncements(filteredAnnouncements);
+    };
+
+    fetchAnnouncements();
     fetchUserInfo();
-  }, [id]);
+  }, [id, userInfo?._id]);
 
   const teamCreated = userInfo?.teams?.map((team) => team.name);
 
-  const filteredAnnouncements = announcements?.filter((announcement) => announcement.user?._id === userInfo?._id);
-console.log("id",userInfo?._id)
   const handleFileChange = (e) => {
     console.log("File selected");
     if (e.target.files && e.target.files.length > 0) {
@@ -66,92 +64,95 @@ console.log("id",userInfo?._id)
 
   return (
     <>
-      <TemplatePage />
-      <section className="middle">
-        <div className={UserProfilePageStyles.topContainer}>
-          <div className={UserProfilePageStyles.profileContainer}>
-            <div className={UserProfilePageStyles.titleBox}>
-              <h2 className={UserProfilePageStyles.title}>
-                Profil utilisateur
-              </h2>
+      <TemplatePage>
+        <section className="middle">
+          <div className={UserProfilePageStyles.topContainer}>
+            <div className={UserProfilePageStyles.profileContainer}>
+              <div className={UserProfilePageStyles.titleBox}>
+                <h2 className={UserProfilePageStyles.title}>
+                  Profil utilisateur
+                </h2>
+              </div>
+              <Fade>
+                {userInfo ? (
+                  <>
+                    <div className={UserProfilePageStyles.middleContainer}>
+                      <div className={UserProfilePageStyles.AnnouncementCard}>
+                        <div className={UserProfilePageStyles.profilePicture}>
+                          <img
+                            src={profilePicture}
+                            alt="Profile"
+                            className={UserProfilePageStyles.picture}
+                            onClick={handlePictureClick}
+                          />
+                          <i
+                            className="fas fa-camera"
+                            onClick={handlePictureClick}
+                          />
+
+                          <input
+                            ref={fileInputRef}
+                            type="file"
+                            accept="image/*"
+                            style={{ display: "none" }}
+                            onChange={handleFileChange}
+                          />
+                        </div>
+
+                        <div className={UserProfilePageStyles.teamName}>
+                          <h1>
+                            {userInfo.username} -{" "}
+                            {userInfo.rank || "aucun rank"}
+                          </h1>
+
+                          <p>Mail : {userInfo.mail || "non renseigné"}</p>
+                          <p>Discord : {userInfo.discord || "non renseigné"}</p>
+                          <p>Valorant ID: {userInfo.valorant_id}</p>
+                          <p>
+                            team(s) created:{" "}
+                            {teamCreated || "aucune équipe crée"}
+                          </p>
+                          <p>region : {userInfo?.region || "non renseigné"}</p>
+                          <p>
+                            Disponibilités :
+                            {userInfo?.availability?.join() || "non renseigné"}
+                          </p>
+                          <p>
+                            Date de création :
+                            {moment(userInfo.createdAt).format("lll")}
+                          </p>
+                          <p>
+                            Dernière mise à jour :
+                            {moment(userInfo.updatedAt).format("lll")}
+                          </p>
+                        </div>
+                        <div className={UserProfilePageStyles.modifyIcon}>
+                          <i className="fas fa-pen" />
+                        </div>
+                      </div>
+
+                      {announcements?.map((announcement, index) => (
+                        <AnnouncementCard
+                          key={index}
+                          announcement={announcement}
+                        />
+                      ))}
+                      <div className={UserProfilePageStyles.button}>
+                        <Button text="Publier une annonce" />
+                        <Button text="Créer son équipe" />
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <span style={{ marginLeft: "3%" }}>
+                    Chargement des données...
+                  </span>
+                )}
+              </Fade>
             </div>
-            <Fade>
-              {userInfo ? (
-                <>
-                  <div className={UserProfilePageStyles.middleContainer}>
-                    <div className={UserProfilePageStyles.AnnouncementCard}>
-                      <div className={UserProfilePageStyles.profilePicture}>
-                        <img
-                          src={profilePicture}
-                          alt="Profile"
-                          className={UserProfilePageStyles.picture}
-                          onClick={handlePictureClick}
-                        />
-                        <i
-                          className="fas fa-camera"
-                          onClick={handlePictureClick}
-                        />
-
-                        <input
-                          ref={fileInputRef}
-                          type="file"
-                          accept="image/*"
-                          style={{ display: "none" }}
-                          onChange={handleFileChange}
-                        />
-                      </div>
-
-                      <div className={UserProfilePageStyles.teamName}>
-                        <h1>
-                          {userInfo.username} - {userInfo.rank || "aucun rank"}
-                        </h1>
-
-                        <p>Mail : {userInfo.mail || "non renseigné"}</p>
-                        <p>Discord : {userInfo.discord || "non renseigné"}</p>
-                        <p>Valorant ID: {userInfo.valorant_id}</p>
-                        <p>
-                          team(s) created: {teamCreated || "aucune équipe crée"}
-                        </p>
-                        <p>region : {userInfo?.region || "non renseigné"}</p>
-                        <p>
-                          Disponibilités :
-                          {userInfo?.availability?.join() || "non renseigné"}
-                        </p>
-                        <p>
-                          Date de création :
-                          {moment(userInfo.createdAt).format("lll")}
-                        </p>
-                        <p>
-                          Dernière mise à jour :
-                          {moment(userInfo.updatedAt).format("lll")}
-                        </p>
-                      </div>
-                      <div className={UserProfilePageStyles.modifyIcon}>
-                        <i className="fas fa-pen" />
-                      </div>
-                    </div>
-
-                    {filteredAnnouncements ?.map((announcement, index) => (
-                      <AnnouncementCard
-                        key={index}
-                        announcement={announcement}
-                      />
-                    ))}
-                    <div className={UserProfilePageStyles.button}>
-                      <Button text="Publier une annonce" />
-                      <Button text="Créer son équipe" />
-                    </div>
-                  </div>
-                </>
-              ) : (
-                <span style={{ marginLeft: "3%" }}>
-                  Chargement des données...
-                </span>
-              )}
-            </Fade>
           </div>
-        </div>
-      </section>
+        </section>
+      </TemplatePage>
     </>
   );
 };
